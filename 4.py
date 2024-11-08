3b0ef2feecfbc4be68a4e60c9214af0f138c5c76
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-11-08 11:00:25 +0000

    funzione POST fatta

--- 4.py
+++ 4.py
@@ -23,5 +23,29 @@ def get_animali():
     connessione.close()
     return jsonify(animali)
 
+@app.route('/api/animali', methods=['POST'])
+def add_animali():
+    data = request.get_json()
+    if not all(key in data for key in ('nome_comune', 'ordine', 'dimensioni', 'habitat', 'alimentazione')):
+        return jsonify({"error": "Dati mancanti"}), 400
+
+    nome = data['nome_comune']
+    ordine = data['ordine']
+    dimensioni = data['dimensioni']
+    habitat = data['habitat']
+    alimentazione = data['alimentazione']
+
+    connessione = connetti_db()
+    cursore = connessione.cursor()
+
+    sql = "INSERT INTO mammiferi (nome_comune, ordine, dimensioni, habitat, alimentazione) VALUES (%s, %s, %s, %s, %s)"
+    cursore.execute(sql, (nome, ordine, dimensioni, habitat, alimentazione))
+    connessione.commit()
+
+    cursore.close()
+    connessione.close()
+
+    return jsonify({"message": "Animale inserito con successo!"}), 201
+
 if __name__ == '__main__':
     app.run(debug=True)
