00b0879d12bc336625a21f47274850af3563b707
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-11-08 11:05:05 +0000

    commit 4 e 6 fatti

--- 4.py
+++ 4.py
@@ -47,5 +47,53 @@ def add_animali():
 
     return jsonify({"message": "Animale inserito con successo!"}), 201
 
+@app.route('/api/animali/<int:id>', methods=['PUT'])
+def update_mammifero(id):
+    data = request.get_json()
+    if not all(key in data for key in ('nome_comune', 'ordine', 'dimensioni', 'habitat', 'alimentazione')):
+        return jsonify({"error": "Dati mancanti"}), 400
+
+    nome = data['nome_comune']
+    ordine = data['ordine']
+    dimensioni = data['dimensioni']
+    habitat = data['habitat']
+    alimentazione = data['alimentazione']
+
+    connessione = connetti_db()
+    cursore = connessione.cursor()
+
+    sql = "UPDATE mammiferi SET nome_comune = %s, ordine = %s, dimensioni = %s, habitat = %s, alimentazione = %s WHERE Id = %s"
+    cursore.execute(sql, (nome, ordine, dimensioni, habitat, alimentazione, id))
+    connessione.commit()
+
+    rowcount = cursore.rowcount
+    cursore.close()
+    connessione.close()
+
+    if rowcount == 0:
+        return jsonify({"message": "Animale non trovato"}), 404
+
+    return jsonify({"message": "Animale aggiornato con successo!"}), 200
+
+@app.route('/api/animali/<int:id>', methods=['DELETE'])
+def delete_mammifero(id):
+    connessione = connetti_db()
+    cursore = connessione.cursor()
+
+    cursore.execute("SELECT * FROM mammiferi WHERE Id = %s", (id,))
+    if not cursore.fetchone():
+        cursore.close()
+        connessione.close()
+        return jsonify({"message": "Animale non trovato"}), 404
+
+    cursore.execute("DELETE FROM mammiferi WHERE Id = %s", (id,))
+    connessione.commit()
+
+    cursore.close()
+    connessione.close()
+
+    return jsonify({"message": "Animale eliminato con successo!"}), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
+
